9c79d571c2c9a4d516d55ab0ed8507a5bf0ce2ae
Thomas Ludwig <moonmaker@gmx.de>
2012-02-24 00:04:37 +0100

    + ‘CS373/Unit-1/unit-1_10.py’

---
+++ CS373/Unit-1/unit-1_10.py
@@ -0,0 +1,15 @@
+# Uniform-Probability-Quiz 
+# Robot Localization
+
+# We have 5 different cells where each cell has the same probability that the robot might be in that cell.
+# So probabilities add up to 1
+# Quiz from x1 to x5
+# -> What is the probability of any of those x's?
+p=[]
+n=5
+for i in range(n):
+	p.append(1)
+
+print p
+
+
