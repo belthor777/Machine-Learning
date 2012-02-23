fa45b31c815b4d8d7f07a40486a4f8a8cbae7a70
Thomas Ludwig <moonmaker@gmx.de>
2012-02-23 22:52:29 +0100

    / ‘CS373/Unit-1/unit-1_4.py’

--- CS373/Unit-1/unit-1_4.py
+++ CS373/Unit-1/unit-1_4.py
@@ -1,6 +1,6 @@
 # Uniform-Probability-Quiz 
 # Robot Localization
 
-# 
+# We have 5 different cells where each cell has the same probability that the robot might be in that cell.
 n=5
 p = [1./n]*n
