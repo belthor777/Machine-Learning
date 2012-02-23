5a7b20a428bd66b5064ef3dd86482018a94b0c70
Thomas Ludwig <moonmaker@gmx.de>
2012-02-23 23:04:52 +0100

    + ‘CS373/Unit-1/unit-1_5.py’

---
+++ CS373/Unit-1/unit-1_5.py
@@ -0,0 +1,12 @@
+# Uniform-Probability-Quiz 
+# Robot Localization
+
+# We have 5 different cells where each cell has the same probability that the robot might be in that cell.
+# So probabilities add up to 1
+# Quiz from x1 to x5
+# -> What is the probability of any of those x's?
+import decimal
+
+n = decimal.Decimal(5)
+p_xi = 1/n
+print "-> The probability of any x's is %f" % p_xi
