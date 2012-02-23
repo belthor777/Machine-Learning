586e28fbb052b726856277397c3f33c1ad3c5b2a
Thomas Ludwig <moonmaker@gmx.de>
2012-02-23 22:54:18 +0100

    / ‘CS373/Unit-1/unit-1_4.py’

--- CS373/Unit-1/unit-1_4.py
+++ CS373/Unit-1/unit-1_4.py
@@ -4,6 +4,6 @@
 # We have 5 different cells where each cell has the same probability that the robot might be in that cell.
 # So probabilities add up to 1
 # Quiz from x1 to x5
-# -> What is the probability of
+# -> What is the probability ofany of those x's?
 n=5
-p = [1./n]*n
+p(xi) = [1./n]*n
