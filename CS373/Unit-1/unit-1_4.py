d33c0d014835f8a436cac49dd5360a98d52e605b
Thomas Ludwig <moonmaker@gmx.de>
2012-02-23 23:00:48 +0100

    / ‘CS373/Unit-1/unit-1_4.py’

--- CS373/Unit-1/unit-1_4.py
+++ CS373/Unit-1/unit-1_4.py
@@ -4,9 +4,9 @@
 # We have 5 different cells where each cell has the same probability that the robot might be in that cell.
 # So probabilities add up to 1
 # Quiz from x1 to x5
-# -> What is the probability ofany of those x's?
+# -> What is the probability of any of those x's?
 import decimal
 
 n = decimal.Decimal(5)
 p_xi = 1/n
-print p_xi
+print "The probability of any of x's is %d" % p_xi
