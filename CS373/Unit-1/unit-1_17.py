58197725921f798c2cc9696a31d2a9cc7575472f
Thomas Ludwig <moonmaker@gmx.de>
2012-02-29 22:19:27 +0100

    + ‘CS373/Unit-1/unit-1_17.py’ - ‘CS373/Unit-1/.goutputstream-XD6CAW’

--- CS373/Unit-1/unit-1_17.py
+++ CS373/Unit-1/unit-1_17.py
@@ -1,20 +1,27 @@
-# Uniform-Probability-Quiz 
-# Robot Localization
+#Program a function that returns a new distribution 
+#q, shifted to the right by U units. If U=0, q should 
+#be the same as p.
 
-p=[0.2, 0.2, 0.2, 0.2, 0.2]
+p=[0, 1, 0, 0, 0]
 world=['green', 'red', 'red', 'green', 'green']
-Z = 'red'
+measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
 
-def sense(p, Z)
-	q=[]
-	n=len(p)
-	for i in range(n):
-		hit = Z == world[i])
-		q.append(p[i] * (hit * pHit + (1-hit) * pMiss) )
-	return q
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
 
-print sense(p, Z)
+def move(p, U):
+	q= []
+	for i in range(len(p)):
+		q.append(p
+    return p
 
-
+print move(p, -1)
