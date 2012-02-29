aa4a8540422e1aebb222936a8ce7a0c77e25d941
Thomas Ludwig <moonmaker@gmx.de>
2012-02-29 22:46:25 +0100

    / ‘CS373/Unit-1/unit-1_21.py’

--- CS373/Unit-1/unit-1_21.py
+++ CS373/Unit-1/unit-1_21.py
@@ -1,12 +1,15 @@
-#Program a function that returns a new distribution 
-#q, shifted to the right by U units. If U=0, q should 
-#be the same as p.
+#Write code that makes the robot move twice and then prints 
+#out the resulting distribution, starting with the initial 
+#distribution p = [0, 1, 0, 0, 0]
 
 p=[0, 1, 0, 0, 0]
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q=[]
@@ -19,10 +22,14 @@ def sense(p, Z):
     return q
 
 def move(p, U):
-    q=[]
-    n=len(p)
-    for i in range(n):
-       q.append( p[(i-U) % n] )
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
     return q
-
-print move(p, -1)
+#
+# ADD CODE HERE
+#
+print move(p,1)
